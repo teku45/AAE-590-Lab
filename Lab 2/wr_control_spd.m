9f2f9dd9ccbdc5782464ceec53a4e3887546a6b4
Sidd Subramanyam <subram32@purdue.edu>
2022-09-14 21:08:11 -0400

    Revert "Upload from Lab"
    This reverts commit 23ac178aef0ad6b60743012a30ee438f470f42f3.

--- Lab 2/wr_control_spd.m
+++ Lab 2/wr_control_spd.m
@@ -5,14 +5,14 @@ function [wr] = wr_control_spd(wr, time)
     PWML = 0;
     PWMR = 0;
 
-    Kp = 0.0305;
-    Ki = 0.006722;
-    Kd = 0.00047;
+    Kp = 0.805;
+    Ki = 3.722;
+    Kd = 0.037;
 
-    %v_curr = pdist([wr.pos; wr.pos_old]) / time.dt
-    v_curr = sqrt((wr.pos(2)-wr.pos_old(2))^2 + (wr.pos(1)-wr.pos_old(1))^2) / time.dt
+    % setting the PWM limits, keep the codes here
+    v_curr = pdist([wr.pos; wr.pos_old]) / time.dt;
     wr.espd = wr.forward_spd - v_curr;
-    %[wr.espd]
+
     espd_deriv = (wr.espd -  wr.espd_old) / time.dt;
     wr.espd_cum = wr.espd_cum + wr.espd;
     PWM = Kp * wr.espd + Ki * wr.espd_cum + Kd * espd_deriv;
@@ -25,20 +25,8 @@ function [wr] = wr_control_spd(wr, time)
     PWML = PWM;
     PWMR = PWM;
 
-    if isnan(wr.espd_cum)
-        wr.espd_cum=0;
-    end
-
-    % setting the PWM limits, keep the codes here
     PWML = min(254, PWML);
     wr.PWML = uint8(max(0, PWML));
     PWMR = min(254, PWMR);
     wr.PWMR = uint8(max(0, PWMR));
-    PWML;
-    wr.pos_old = wr.pos;
-
-
-    scatter(time.curr,v_curr,'k'); hold on;
-    scatter(time.curr,wr.forward_spd,'r');
-    drawnow;
 end
--- Lab 2/wr_control_wp.m
+++ Lab 2/wr_control_wp.m
@@ -15,20 +15,17 @@ function [wr] = wr_control_wp(wr, time)
     target_wayP = wr.WP(wr.curWP,:);
 
     % Heading angle
-    desired_angle = atand(target_wayP(2) - wr.pos(2))/( target_wayP(1) - wr.pos(1)));
-    actual_angle = atand((wr.heading_vec(2), wr.heading_vec(1));
-    wr.heading_dir = [cosd(desired_angle), sind(desired_angle)];
+    desired_angle = atan2d(target_wayP(2) - wr.pos(2), target_wayP(1) - wr.pos(1));
+    actual_angle = atan2d(wr.heading_vec(2), wr.heading_vec(1));
+    wr.heading_dir = [cos(desired_angle), sin(desired_angle)];
     e_heading = desired_angle - actual_angle;
-    [actual_angle desired_angle e_heading]
+
     % Call heading func if the error is outside the error margin
     % Call speed func if error is outside error margin
     if (e_heading > 15)
         wr = wr_control_heading(wr, time);
-        %fprintf("heading mode");
-    elseif (sqrt((wr.pos(2)-target_wayP(2))^2 + (wr.pos(1)-target_wayP(1))^2) > 100) 
-        
+    elseif (pdist([wr.pos; target_wayP]) > 100) 
         wr = wr_control_spd(wr, time);
-        %fprintf("speed mode");
     else
         wr.curWP = wr.curWP + 1;
     end
